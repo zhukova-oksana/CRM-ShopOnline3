800d9cb4ac78792b56859621ac91e666545fdf8c
zhukova_oksana <ksy2905@gmail.com>
2022-08-03 19:17:43 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const nameGoods = prompt('Наименование товара: ');
-const quantityGoods = prompt('Количество товара: ');
+let quantityGoods = prompt('Количество товара: ');
 const categoryGoods = prompt('Категория товара: ');
-const priceGoods = prompt('Цена товара: ');
+let priceGoods = prompt('Цена товара: ');
+
+quantityGoods = +quantityGoods;
+priceGoods = +priceGoods;
 
 
-console.log('Тип данных поля "Наименование товара"', typeof(nameGoods));
-console.log('Тип данных поля "Количество товара"', typeof(Number(quantityGoods)));
-console.log('Тип данных поля "Категория товара"', typeof(categoryGoods));
-console.log('Тип данных поля "Цена товара"', typeof(Number(priceGoods)));
+console.log('Тип данных поля "Наименование товара"', typeof nameGoods);
+console.log('Тип данных поля "Количество товара"', typeof quantityGoods);
+console.log('Тип данных поля "Категория товара"', typeof categoryGoods);
+console.log('Тип данных поля "Цена товара"', typeof priceGoods);
 
 console.log(`На складе ${quantityGoods} единицы товара "${nameGoods}" на сумму ${quantityGoods * priceGoods} деревянных`);
